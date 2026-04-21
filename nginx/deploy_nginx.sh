52009c46c50917bcdc04c6471d9f08900c5554a1
Laura Sanchez Urda <laurasa@tbworkers3.esi.uclm.es>
2026-04-21 20:21:48 +0200

    Finalización parte Nginx

--- nginx/deploy_nginx.sh
+++ nginx/deploy_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Instalación automática de Nginx
-sudo apt-get update && sudo apt-get install -y nginx
+#Aprovisionamiento automático y plataforma
+sudo apt-get update
+sudo apt-get install -y nginx php-fpm php-mysql # php-mysql es clave para el CMS
 
 # Configuración según el tipo de nodo
 if [[ $(hostname) == *"lb"* ]]; then
@@ -9,7 +10,7 @@ else
     sudo cp nginx_frontal.conf /etc/nginx/sites-available/default
 fi
 
-#Securización (Solo puertos necesarios)
+#Securización (UFW)
 sudo ufw allow 80/tcp
 sudo ufw allow 22/tcp
 sudo ufw --force enable
